a8feea47d7a7f661295530906899cceab0a17dd5
Athena Crawley <athenacrawley@gmail.com>
2024-11-24 10:58:02 +0000

    made it not loop infinitely woohoo (also, milk)

--- Program.cs
+++ Program.cs
@@ -1,51 +1,84 @@
 ﻿using System;
 internal class Program
 {
-    private static void Main(string[] args)
+    private static void Main()
     {
         Console.WriteLine("" +
-    "Welcome to the Macbeth Key Quote Tester \n" +
-    "Type the number of the act you would like to be tested on, or \"all\" for all acts. \n" +
-    "---------------------------------------------------------------------------------");
-        string actchoice = "";
-        actchoice = Console.ReadLine();
+            "Welcome to the Macbeth Key Quote Tester \n" +
+            "Type the number of the act you would like to be tested on, or \"all\" for all acts. \n" +
+            "If you want to quit, type \"quit\". \n" +
+            "---------------------------------------------------------------------------------");
+        string actchoice = Console.ReadLine()!;
+        bool continueLooping = true;
+        int result;
 
-        if (actchoice == "1")
-            Console.WriteLine("" +
-                "You have chosen Act " + actchoice, ". \n" +
-                "Finish the quote, including punctuation.")
-        Console.WriteLine();
-
-        else if (actchoice == "2")
-            Console.WriteLine("" +
-                "You have chosen Act " + actchoice, ". \n" +
-                "Finish the quote, including punctuation.");
-
-        else if (actchoice == "3")
-            Console.WriteLine("" +
-                "You have chosen Act " + actchoice, ". \n" +
-                "Finish the quote, including punctuation.");
-
-        else if (actchoice == "4")
-            Console.WriteLine("" +
-                "You have chosen Act " + actchoice, ". \n" +
-                "Finish the quote, including punctuation.");
-
-        else if (actchoice == "5")
-            Console.WriteLine("" +
-                "You have chosen Act " + actchoice, ". \n" +
-                "Finish the quote, including punctuation.");
-
-        else if (actchoice == "all")
-            Console.WriteLine("" +
-                "You have chosen All Acts. \n" +
-                "Finish the quote, including punctuation.");
-        else if (actchoice == "quit")
-            Environment.Exit(0);
-        else
+        while (continueLooping == true)
         {
-            Console.WriteLine("" +
-                "Please type a number between 1-5, \"all\" or \"quit.\"");
+            if (actchoice == "1")
+            {
+                Console.WriteLine("" +
+                    "You have chosen Act " + actchoice, ". \n" +
+                    "Finish the quotes, including punctuation.");
+                continueLooping = false;
+            }
+            else if (actchoice == "2")
+            {
+                Console.WriteLine("" +
+                    "You have chosen Act " + actchoice, ". \n" +
+                    "Finish the quotes, including punctuation.");
+                continueLooping = false;
+            }
+
+            else if (actchoice == "3")
+            {
+                Console.WriteLine("" +
+                    "You have chosen Act " + actchoice, ". \n" +
+                    "Finish the quotes, including punctuation.");
+                continueLooping = false;
+            }
+
+            else if (actchoice == "4")
+            {
+                Console.WriteLine("" +
+                    "You have chosen Act " + actchoice, ". \n" +
+                    "Finish the quotes, including punctuation.");
+                continueLooping = false;
+            }
+
+            else if (actchoice == "5")
+            {
+                Console.WriteLine("" +
+                    "You have chosen Act " + actchoice, ". \n" +
+                    "Finish the quotes, including punctuation.");
+                continueLooping = false;
+            }
+
+            else if (actchoice == "all")
+            {
+                Console.WriteLine("" +
+                    "You have chosen all Acts. \n" +
+                    "Finish the quotes, including punctuation.");
+                continueLooping = false;
+            }
+
+            else if (actchoice == "milk")
+            {
+                Console.WriteLine("" +
+                    "lady macbeth milkies...");
+            }
+
+            else if (actchoice == "quit")
+            {
+                continueLooping = false;
+                Environment.Exit(0);
+            }
+            else
+            {
+                Console.WriteLine("" +
+                    "Please type a number between 1-5, \"all\" or \"quit.\"");
+                actchoice = Console.ReadLine()!;
+            }
         }
+
     }
 }
